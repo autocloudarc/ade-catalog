b92881037744955d37e56c036157b92b2831e62b
autocloudarc <autocloudarc@outlook.com>
2024-09-21 08:33:55 -0400

    tfvars(json): remove

--- src/Environments/tf-deployment/variables.tf
+++ src/Environments/tf-deployment/variables.tf
@@ -1,27 +1,34 @@
 variable "resourceGroupName" {
     type = string
+    default = "ade-tfm-rgp-01"
 }   
 
 variable "storageAccountPrefix" {
     type = string
+    default = "1sta"
 }
 
 variable "logAnalyticsWorkspaceName" {
     type = string
+    default = "tfm-law-01"
 }
 
 variable "web_nsg" {
     type = string
+    default = "web-nsg"
 }
 
 variable "app_nsg" {
     type = string
+    default = "app-nsg"
 }
 
 variable "dta_nsg" {
   type = string
+  default = "dta-nsg"
 }
 
 variable "location" {
     type = string
+    default = "eastus2"
 }
