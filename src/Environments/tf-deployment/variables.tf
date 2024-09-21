0a1287acdb30638669ee019cd8929aa3b05879e8
autocloudarc <autocloudarc@outlook.com>
2024-09-21 08:26:06 -0400

    variables(tf): add

---
+++ src/Environments/tf-deployment/variables.tf
@@ -0,0 +1,27 @@
+variable "resourceGroupName" {
+    type = string
+}   
+
+variable "storageAccountPrefix" {
+    type = string
+}
+
+variable "logAnalyticsWorkspaceName" {
+    type = string
+}
+
+variable "web_nsg" {
+    type = string
+}
+
+variable "app_nsg" {
+    type = string
+}
+
+variable "dta_nsg" {
+  type = string
+}
+
+variable "location" {
+    type = string
+}
