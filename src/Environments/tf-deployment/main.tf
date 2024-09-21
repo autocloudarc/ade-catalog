d0793fe6317abe8b956a7ccf47666b52db35e110
autocloudarc <autocloudarc@outlook.com>
2024-09-21 07:57:22 -0400

    vars(declarations): place directly in main.tf

--- src/Environments/tf-deployment/main.tf
+++ src/Environments/tf-deployment/main.tf
@@ -11,6 +11,34 @@ provider "azurerm" {
     features {}
 }
 
+variable "resourceGroupName" {
+    type = string
+}   
+
+variable "storageAccountPrefix" {
+    type = string
+}
+
+variable "logAnalyticsWorkspaceName" {
+    type = string
+}
+
+variable "web_nsg" {
+    type = string
+}
+
+variable "app_nsg" {
+    type = string
+}
+
+variable "dta_nsg" {
+  type = string
+}
+
+variable "location" {
+    type = string
+}
+
 resource "random_string" "random_suffix" {
   length  = 8
   special = false
