231b5f3888a3893622b0b602bcc88402e908dc6c
autocloudarc <autocloudarc@outlook.com>
2024-10-02 21:23:53 -0400

    yml(variables): add to variables.tf

--- src/Environments/tf-deployment/variables.tf
+++ src/Environments/tf-deployment/variables.tf
@@ -1,24 +1,29 @@
 variable "storageAccountPrefix" {
   description = "Prefix for the storage account name"
   type        = string
+  default     = "1sta"
 }
 
 variable "web_nsg" {
   description = "Web network security group name"
   type        = string
+  default     = "web-nsg"
 }
 
 variable "app_nsg" {
   description = "App network security group name"
   type        = string
+  default     = "app-nsg"
 }
 
 variable "dta_nsg" {
   description = "Data network security group name"
   type        = string
+  default     = "dta-nsg"
 }
 
 variable "logAnalyticsWorkspaceName" {
   description = "Log Analytics workspace name"
   type        = string
+  default     = "arm-law-01"
 }
